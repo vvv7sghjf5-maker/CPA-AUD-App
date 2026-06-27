1c18133d3604c0741675ef4b6344225e24f409eb
vvv7sghjf5-maker <vvv7sghjf5@privaterelay.appleid.com>
2026-06-27 13:39:23 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='aud-v8-1-7-cache';
+const CACHE='aud-v8-1-8-cache';
 const ASSETS=['./','./index.html','./script.js','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
